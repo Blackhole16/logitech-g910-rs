36d6d971aea79d61188f6c6f6928e6483145ea28
Blackhole16 <Blackhole16@web.de>
2016-04-14 16:31:44 +0200

    wip(utils): read from device
    utils: add open_device
           read_device, which is wip

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,15 @@ fn main() {
     let product_id: u16 = argv[2].parse().unwrap();
     println!("Vendor-Id: {}    Product-Id: {}", vendor_id, product_id);
 
-    let context = utils::get_context();
-
+    let mut context = utils::get_context();
+    let (mut device, device_desc, mut handle) =
+        match utils::open_device(&mut context, &vendor_id, &product_id) {
+            Ok(t) => t,
+            Err(e) => panic!("Error finding / opening device: {}", e),
+    };
+    match utils::read_device(&mut device, &device_desc, &mut handle) {
+        Ok(_) => println!("Finished"),
+        Err(e) => panic!("Cannot read from Device: {}", e),
+    }
 }
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,13 @@
 use libusb::{
+    LogLevel,
     Context,
     Device,
     DeviceDescriptor,
-    LogLevel,
+    DeviceHandle,
+    Result,
+    Error,
 };
+use std::time::Duration;
 
 pub fn get_context() -> Context {
     let mut context = match Context::new() {
@@ -18,15 +22,41 @@ pub fn get_context() -> Context {
     return context;
 }
 
-pub fn get_device(context: &mut Context, vendor_id: u16, product_id: u16) -> Option<Device> {
-    for mut d in context.devices().unwrap().iter() {
+pub fn open_device<'a>(context: &'a mut Context, vendor_id: &u16, product_id: &u16) -> Result<(Device<'a>, DeviceDescriptor, DeviceHandle<'a>)> {
+    let devices = match context.devices() {
+        Ok(devices) => devices,
+        Err(e) => return Err(e),
+    };
+    for mut d in devices.iter() {
         let dd = match d.device_descriptor() {
             Ok(dd) => dd,
             Err(_) => continue
         };
-        if dd.product_id() == product_id && dd.vendor_id() == vendor_id {
-            return Some(d);
+        if dd.vendor_id() == *vendor_id && dd.product_id() == *product_id  {
+            return match d.open() {
+                Ok(handle) => Ok((d, dd, handle)),
+                Err(e) => Err(e),
+            }
         }
     }
-    return None;
+    return Err(Error::NoDevice);
+}
+
+pub fn read_device(device: &mut Device, device_desc: &DeviceDescriptor, handle: &mut DeviceHandle) -> Result<()> {
+    try!(handle.reset());
+
+    let timeout = Duration::from_secs(1);
+    let languages = try!(handle.read_languages(timeout));
+    println!("Active Configuration: {}", try!(handle.active_configuration()));
+    println!("Languages: {:?}", languages);
+
+    if languages.len() > 0 {
+        let lang = languages[0];
+        println!("Manufacturer: {}", try!(handle.read_manufacturer_string(lang, device_desc, timeout)));
+        println!("Product: {}", try!(handle.read_product_string(lang, device_desc, timeout)));
+        println!("Serial Number: {}", try!(handle.read_serial_number_string(lang, device_desc, timeout)));
+    }
+
+
+    return Ok(());
 }
