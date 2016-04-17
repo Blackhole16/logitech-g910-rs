2aa036f6a2a5d9f019755b6c52e159aec43e0052
Blackhole16 <jaro.fietz@gmx.de>
2016-04-17 12:02:51 +0200

    wip(utils): Claiming iface 1 and 0, then reading from 0 seems to work for basic input

--- src/utils.rs
+++ src/utils.rs
@@ -139,7 +139,6 @@ pub fn get_endpoints(device: &mut Device, device_desc: &DeviceDescriptor, dir: D
 }
 
 fn read_endpoint(handle: &mut DeviceHandle, endpoint: &Endpoint) -> Result<()>{
-    let s = Duration::from_secs(10);
     let has_kernel_driver = match handle.kernel_driver_active(endpoint.iface) {
         Ok(true) => {
             handle.detach_kernel_driver(endpoint.iface);
@@ -147,33 +146,56 @@ fn read_endpoint(handle: &mut DeviceHandle, endpoint: &Endpoint) -> Result<()>{
         },
         _ => false
     };
-    //try!(handle.reset());
-    println!("    Kernel driver active: {}", has_kernel_driver);
-    //sleep(s);
-    //try!(handle.release_interface(endpoint.iface));
+    println!("    Kernel driver active for iface {}: {}", endpoint.iface, has_kernel_driver);
+    // we also need to be able to write to / read from interface 0, otherwise
+    // set_active_configuration reports a busy device
+    let has_kernel_driver0 = match handle.kernel_driver_active(0) {
+        Ok(true) => {
+            handle.detach_kernel_driver(0);
+            true
+        },
+        _ => false
+    };
+    println!("    Kernel driver active for iface 0: {}", has_kernel_driver);
+    
+    let timeout = Duration::from_secs(1);
+    try!(handle.reset());
+    for lang in handle.read_languages(timeout).unwrap() {
+        println!("Got lang: {:?}", lang);
+        for i in 0..256u8 {
+            handle.read_string_descriptor(lang, 0u8, timeout).map(|s| println!("got desc {}: {}", i, s));
+        }
+    }
     println!("0");
-    try!(handle.claim_interface(endpoint.iface));
-    try!(handle.release_interface(endpoint.iface));
+    //try!(handle.unconfigure());
     println!("1");
-    try!(handle.set_active_configuration(endpoint.config));
-    sleep(s);
+    try!(handle.set_active_configuration(1));
     println!("2");
-    try!(handle.set_alternate_setting(endpoint.iface, endpoint.setting));
+    //try!(handle.claim_interface(endpoint.iface));
+    try!(handle.claim_interface(0));
     println!("3");
-    sleep(s);
+    //try!(handle.set_alternate_setting(endpoint.iface, endpoint.setting));
+    try!(handle.set_alternate_setting(0, 0));
+    println!("4");
 
-    let timeout = Duration::from_secs(5);
-    //let mut vec = Vec::<u8>::with_capacity(256);
-    let mut buf = [0u8; 256];
+    let mut buf = [0u8; 8];
     println!("start reading {} bytes", buf.len());
-    match handle.read_interrupt(endpoint.address, &mut buf, timeout) {
-        Ok(len) => println!("Read {} bytes", len),
-        Err(e) => println!("Error reading bytes: {:?}", e)
+    while match handle.read_interrupt(129, &mut buf, timeout) {
+        Ok(len) => { print!("read {} bytes: ", len); true },
+        Err(e) => { print!("ERROR reading: {:?}", e); false }
+    } {
+        println!("{}, {}, {}, {}, {}, {}, {}, {}", &buf[0], &buf[1], &buf[2], &buf[3], &buf[4], &buf[5], &buf[6], &buf[7]);
     }
 
-    let mut vec = Vec::with_capacity(256);
-    vec.extend_from_slice(&buf);
+    handle.release_interface(endpoint.iface);
+    handle.release_interface(0);
 
-    println!("read: {:?}", &vec);
+    // reattach kernel driver(s)
+    if has_kernel_driver {
+        handle.attach_kernel_driver(endpoint.iface);
+    }
+    if has_kernel_driver0 {
+        handle.attach_kernel_driver(0);
+    }
     return Ok(());
 }
