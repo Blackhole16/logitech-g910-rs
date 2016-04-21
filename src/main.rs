f7199540bfc7f27f5cc446587b1e372ea8a5384c
Blackhole16 <jaro.fietz@gmx.de>
2016-04-22 01:07:10 +0200

    wip(replay): Test control packet replay in main()
    Interpret read pcap packets as stream of IN/OUT Control Packets and
    replay them.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(inclusive_range_syntax)]
+
 extern crate libusb;
 extern crate pcap;
 
@@ -11,8 +13,8 @@ use std::path::Path;
 
 fn main() {
     let mut cap = replay::get_capture(&Path::new("pcap/g602-handshake.pcap"));
-    replay::print(&mut cap);
-
+    //replay::print(&mut cap);
+    cap.next();cap.next();cap.next();cap.next();cap.next();cap.next();
 
     let argv: Vec<String> = std::env::args().collect();
     println!("{:?}", argv);
@@ -31,6 +33,36 @@ fn main() {
             Ok(t) => t,
             Err(e) => panic!("Error finding / opening device: {}", e),
     };
+    use std::io;
+    use std::io::prelude::*;
+    let timeout = std::time::Duration::from_secs(1);
+    let stdin = io::stdin();
+    handle.detach_kernel_driver(0);
+    handle.claim_interface(0);
+    for l in stdin.lock().lines() {
+        let l = l.unwrap();
+        if l == "end" {
+            break;
+        }
+        let mut buf = Vec::new();
+        {
+            let req = usb::Packet::from_bytes(cap.next().unwrap().data).unwrap();
+            buf.resize(req.get_w_length() as usize, 0u8);
+            println!("{:?}", req);
+            println!("{}", buf.len());
+            println!("{}", (&buf[..]).len());
+            println!("{}", handle.read_control(req.get_bm_request_type(), req.get_b_request(), req.get_value(), req.get_language_id(), &mut buf, timeout).unwrap());
+            println!("{:?}", buf);
+        }
+        {
+            let res = usb::Packet::from_bytes(cap.next().unwrap().data).unwrap();
+            println!("{:?}", res);
+            println!("Result correct: {}", res.get_data() == &buf[..]);
+        }
+    }
+    handle.release_interface(0);
+    handle.attach_kernel_driver(0);
+    return;
     match utils::read_device(&mut device, &device_desc, &mut handle) {
         Ok(_) => println!("Finished"),
         Err(e) => panic!("Cannot read from Device: {}", e),
--- src/replay.rs
+++ src/replay.rs
@@ -1,6 +1,8 @@
 use pcap::{Capture, Offline};
 use std::path::Path;
-use usb::Packet;
+use libusb::{DeviceHandle, Result, Device};
+use usb::{Packet, TransferType};
+use std::time::Duration;
 
 pub fn get_capture(path: &Path) -> Capture<Offline> {
     return Capture::from_file(path).unwrap();
@@ -17,3 +19,22 @@ pub fn print(cap: &mut Capture<Offline>) {
 
     }
 }
+
+pub fn handle(device: &mut Device, handle: &mut DeviceHandle, packet: &Packet) -> Result<()> {
+    let timeout = Duration::from_secs(1);
+    match packet.get_transfer_type() {
+        TransferType::Interrupt => {
+            //let buf = packet.get_buf();
+            let buf = &[1u8; 4][..];
+            match handle.write_interrupt(packet.get_endpoint(), buf, timeout) {
+                Ok(len) => {
+                    println!("Wrote {} bytes", len);
+                    return Ok(());
+                },
+                Err(e) => return Err(e)
+            }
+        },
+        _ => unimplemented!()
+    }
+}
+
